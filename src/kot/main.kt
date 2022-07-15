6cd61326dcd829f68d245125300df9007a3ada59
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-15 20:38:04 +0600

    abstract class companion object plus fun filter

--- src/kot/main.kt
+++ src/kot/main.kt
@@ -13,10 +13,16 @@ fun main() {
    // println(Calc.square(5))
   //  println(Calc.lengthOfCircle(1.2F))
 
-    val name= listOf<String>("Zhanybek", "Adina", "Daniel")
+   /* val name= listOf<String>("Zhanybek", "Adina", "Daniel")
     val namesStartsFromA=name.filter { it.startsWith("A") }
     for (name in namesStartsFromA){
-        println(name)
+        println(name)*/
+
+    val numbers= (0..100).toList()
+    val doubledNumbers=numbers.map { numbers: Int-> numbers *2 }
+    for (i in doubledNumbers){
+        println(i)
+    }
     }
 
 
@@ -32,4 +38,3 @@ fun main() {
 
 
 
-}
