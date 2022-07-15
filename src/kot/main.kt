06dbd295055d361057df661622980a186645848b
Bermet <bermet.bainaeva.ch@gmail.com>
2022-07-15 20:22:08 +0600

    abstract class companion object plus fun filter

--- src/kot/main.kt
+++ src/kot/main.kt
@@ -1,7 +1,15 @@
 package kot
 
 fun main() {
+
+    val listOfNumbers= mutableListOf<Int>()
+    for (i in 0..99){
+        listOfNumbers.add(i)
+    }
+    val listOfEvenNumbers:List<Int> =listOfNumbers.filter { it %2==0 }
+    for (i in listOfEvenNumbers)
+        println(i)
     //val calc=Calc()
-    println(Calc.square(5))
-    println(Calc.lengthOfCircle(1.2F))
+   // println(Calc.square(5))
+  //  println(Calc.lengthOfCircle(1.2F))
 }
